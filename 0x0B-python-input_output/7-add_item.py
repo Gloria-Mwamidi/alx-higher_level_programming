7805ec7b34f976b3f318a8e0801b802fc9b28436
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-09-13 13:45:37 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Writes a script that adds all arguments to a Python list"""
+"""
+Writes a script that adds all arguments to a Python list
+"""
 
 import sys
 
@@ -7,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 loads_from_json_file = __import__('6-loads_from_json_file').loads_from_json_file
 
 try:
-    info = loads_from_jason_file("add_item.json")
+    info = loads_from_json_file("add_item.json")
 except FileNotFoundError:
     info = []
 
